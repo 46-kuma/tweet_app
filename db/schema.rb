dedd4a70c377bc17efe9d3a961e80c339800b45d
shirokuma <shirokuma240@gmail.com>
2021-08-17 15:31:10 +0900

    courceXI

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_033229) do
+ActiveRecord::Schema.define(version: 2021_08_17_055806) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_033229) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
+    t.string "password_digest"
   end
 
 end
